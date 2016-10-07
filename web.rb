338b1b64e714530735faf7ff94090b8ce9bff9b9
Noel <terrorer9999@gmail.com>
2016-10-08 02:45:18 +0800

    增加機率

--- web.rb
+++ web.rb
@@ -44,7 +44,7 @@ post '/callback' do
           type: 'text',
           text: text
         }
-        client.reply_message(event['replyToken'], message)
+        client.reply_message(event['replyToken'], message) if 'a' == %w(a b c).sample
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
