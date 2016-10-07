972c4bb76f228400ed0ee6b0628dcddbd82ff1a6
Noel <terrorer9999@gmail.com>
2016-10-08 01:09:31 +0800

    測試中...

--- web.rb
+++ web.rb
@@ -9,35 +9,35 @@ def client
 end
 
 post '/callback' do
-  # body = request.body.read
+  body = request.body.read
 
-  # signature = request.env['HTTP_X_LINE_SIGNATURE']
-  # unless client.validate_signature(body, signature)
-  #   error 400 do 'Bad Request' end
-  # end
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
 
-  # events = client.parse_events_from(body)
-  # events.each { |event|
-  #   case event
-  #   when Line::Bot::Event::Message
-  #     case event.type
-  #     when Line::Bot::Event::MessageType::Text
-  #       message = {
-  #         type: 'text',
-  #         text: event.message['text']
-  #       }
-  #       # client.reply_message(event['replyToken'], message)
-  #       client.reply_message(event['replyToken'], 'WTFWTFWTF')
-  #     when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-  #       response = client.get_message_content(event.message['id'])
-  #       tf = Tempfile.open("content")
-  #       tf.write(response.body)      
-  #     end
-  #   end
-  # }
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        # client.reply_message(event['replyToken'], message)
+        client.reply_message(event['replyToken'], 'WTFWTFWTF')
+      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open("content")
+        tf.write(response.body)      
+      end
+    end
+  }
 
-  # 'OK'
-  request.inspect
+  'OK'
+  # request.inspect
 end
 
 get '/hello' do
